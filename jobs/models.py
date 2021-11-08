bc0414ebd2e5a4ec2f84276f687f659b55387ff7
Clifford <kingsleyclifford2016@gmail.com>
2021-11-08 18:03:03 +0000

    added lines of code

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Company(models.Model):
+    first_name = models.CharField(max_length=150, required=True)
+    last_name = models.CharField(max_length=150, required=True)
+    name = models.CharField(max_length=255, required=True)
+    job_title = models.CharField(max_length=255, required=True)
+    work_email = models.EmailField(max_length=255, unique=True)
+    phonenumber = PhoneNumberField(blank=True)

6e0a300f7bd033bb5d0cc25caa597190978a9e8c
Clifford <kingsleyclifford2016@gmail.com>
2021-11-08 17:44:55 +0000

    added lines of code

--- Backend/urls.py
+++ Backend/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users.views import GoogleLogin, FacebookLogin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/", include("users.api.urls")),
+    path("api/v1/rest-auth/", include("rest_auth.urls")),
+    path("api/v1/rest-auth/registration/", include("rest_auth.registration.urls")),
+    path("api/v1/rest-auth/password/change/", include("rest_auth.urls")),
+    path("api/v1/rest-auth/logout/", include("rest_auth.urls")),
+    path(
+        "accounts/", include("allauth.urls")
+    ),  # Chage this once the frontend dev is started
+    path("auth/google/", GoogleLogin.as_view(), name="google-login"),
+    path("auth/facebook/", FacebookLogin.as_view(), name="facebook-login"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    

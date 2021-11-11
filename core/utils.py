da2475259f6dd5b143e7d2ac3473f27cfdb9214d
Clifford <kingsleyclifford2016@gmail.com>
2021-11-11 13:16:41 +0000

    added a lines of code

--- core/utils.py
+++ core/utils.py
@@ -9,8 +9,10 @@ STRING_LENGTH = 6
 def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
     return "".join(random.choice(chars) for _ in range(length))
 
+
 def profile_image_thumbnail(instance, filename):
     return f"profile_images/{instance.id}/{filename}"
+
 
 def cv_thumbnail(instance, filename):
     return f"cv/{instance.id}/{filename}"

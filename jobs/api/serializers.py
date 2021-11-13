0788f3f11a000a7631d5ae3d64bfbe1ffa93aa4a
Clifford <kingsleyclifford2016@gmail.com>
2021-11-13 18:36:32 +0000

    added lines of code

--- jobs/api/serializers.py
+++ jobs/api/serializers.py
@@ -3,18 +3,18 @@ from rest_framework import serializers
 from jobs.models import Company, Seeker
 
 
-class CompanySerializer(serializers.ModelSerializer):
-    slug = serializers.StringRelatedField(read_only=True)
-    seekers = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Company
-        fields = "__all__"
-
-
 class SeekerSerializer(serializers.ModelSerializer):
-    company = CompanySerializer(read_only=True)
+    company = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Seeker
+        fields = "__all__"
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    slug = serializers.StringRelatedField(read_only=True)
+    seekers = SeekerSerializer(many=True)
+
+    class Meta:
+        model = Company
         fields = "__all__"

5b2b21e8a03787e729536f3f8c6de909517bc918
Clifford <kingsleyclifford2016@gmail.com>
2021-11-13 18:50:40 +0000

    added lines of code

--- jobs/api/urls.py
+++ jobs/api/urls.py
@@ -5,9 +5,9 @@ from django.urls import path
 
 urlpatterns = [
     path('companies/', CompanyListView.as_view(), name="companies"),
-    path('companies/', CompanyCreateView.as_view(), name="create-company"),
+    path('create-company/', CompanyCreateView.as_view(), name="create-company"),
     path('companies/<slug>/', CompanyDetailView.as_view(), name="company"),
     path('seekers/', SeekerListView.as_view(), name="seekers"),
-    path('seekers/', SeekerCreateView.as_view(), name="create-seeker"),
+    path('create-seeker/', SeekerCreateView.as_view(), name="create-seeker"),
     path('seekers/<slug>/', SeekerDetailView.as_view(), name="seeker"),
 ]

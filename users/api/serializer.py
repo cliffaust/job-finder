2c730ab3d5011c3bdd7587f936626629db879ff1
Clifford <kingsleyclifford2016@gmail.com>
2021-11-14 15:18:00 +0000

    added lines of code

--- users/api/serializer.py
+++ users/api/serializer.py
@@ -3,7 +3,7 @@ from allauth.utils import email_address_exists
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from rest_framework import serializers
-from users.models import CustomUser
+from users.models import CustomUser, Company
 from django.conf import settings
 
 
@@ -68,3 +68,63 @@ class RegisterSerializer(serializers.ModelSerializer):
         setup_user_email(request, user, [])
         user.save()
         return user
+
+
+class CompanySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Company
+        fields = ['id', 'email', 'company_name', 'profile_pic']
+
+
+class RegisterCompanySerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            "email",
+            "company_name",
+            "profile_pic",
+            "password1",
+            "password2",
+        ]
+
+    def validate_email(self, email):
+        email = get_adapter().clean_email(email)
+        if allauth_settings.UNIQUE_EMAIL:
+            if email and email_address_exists(email):
+                raise serializers.ValidationError(
+                    ("A user is already registered with this e-mail address.", )
+                )
+        return email
+
+    def validate_password1(self, password):
+        return get_adapter().clean_password(password)
+
+    def validate(self, data):
+        if data["password1"] != data["password2"]:
+            raise serializers.ValidationError(("The two password fields didn't match.", ))
+        return data
+
+    def get_cleaned_data(self):
+        return {
+            "company_name": self.validated_data.get("company_name", ""),
+            "profile_pic": self.validated_data.get("profile_pic", ""),
+            "password1": self.validated_data.get("password1", ""),
+            "email": self.validated_data.get("email", ""),
+        }
+
+    def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        adapter.save_user(request, user, self)
+        if self.cleaned_data.get("profile_pic"):
+            user.profile_pic = self.cleaned_data.get("profile_pic")
+        setup_user_email(request, user, [])
+        user.save()
+        return user
+

32fba5ceb87c5e75f898f5dd2b24ab8dc9775df2
Clifford <kingsleyclifford2016@gmail.com>
2021-11-14 21:08:59 +0000

    added lines of code

--- company/api/urls.py
+++ company/api/urls.py
@@ -3,8 +3,15 @@ from .views import CompanyListView, CompanyCreateView, CompanyDetailView, Compan
     CompanyProfileCreateView, CompanyProfileDetailView, CompanyProfileImageListView, CompanyProfileImageCreateView, \
     CompanyProfileImageDetailView
 
-
 urlpatterns = [
     path('companies', CompanyListView.as_view(), name="companies"),
-    path('com')
+    path('companies/<slug>/', CompanyDetailView.as_view(), name="company"),
+    path('create-company/', CompanyCreateView.as_view(), name="create-company"),
+    path('company-profiles/', CompanyProfileListView.as_view(), name="company-profiles"),
+    path('company-profiles/<slug>/', CompanyProfileDetailView.as_view(), name="company-profile"),
+    path('companies/<slug>/create-company-profile/', CompanyProfileCreateView.as_view(), name="create-company-profile"),
+    path('company-profile-images', CompanyProfileImageListView.as_view(), name="company-profile-images"),
+    path('company-profile-images/<int:pk>/', CompanyProfileImageDetailView.as_view(), name="company-profile-image"),
+    path('company-profiles/<slug>/create-company-profile-image/', CompanyProfileImageCreateView.as_view(),
+         name="create-company-profile-image")
 ]

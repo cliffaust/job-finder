a2653b20dc532d7da003de65a16ecfe715e8de4c
Clifford <kingsleyclifford2016@gmail.com>
2021-11-14 15:31:28 +0000

    added lines of code

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserProfileDetailView, UserProfileView
+from .views import UserProfileDetailView, UserProfileView, CompanyProfileDetailView, CompanyProfileView
 
 urlpatterns = [
+    path("company/", UserProfileView.as_view(), name="user"),
+    path("company/<int:pk>/", UserProfileDetailView.as_view(), name="user-detail"),
     path("user/", UserProfileView.as_view(), name="user"),
     path("user/<int:pk>/", UserProfileDetailView.as_view(), name="user-detail")
 ]

92f68791da1dcdecb3e2b3efa1d3df98628a968a
Clifford <kingsleyclifford2016@gmail.com>
2021-11-16 19:28:17 +0000

    added lines of code

--- company/api/urls.py
+++ company/api/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('create-company/', CompanyCreateView.as_view(), name="create-company"),
     path('company-profiles/', CompanyProfileListView.as_view(), name="company-profiles"),
     path('company-profiles/<slug>/', CompanyProfileDetailView.as_view(), name="company-profile"),
-    path('companies/<slug>/create-company-profile/', CompanyProfileCreateView.as_view(), name="create-company-profile"),
+    path('companies/<company_slug>/create-company-profile/', CompanyProfileCreateView.as_view(), name="create-company-profile"),
     path('company-profile-images/', CompanyProfileImageListView.as_view(), name="company-profile-images"),
     path('company-profile-images/<int:pk>/', CompanyProfileImageDetailView.as_view(), name="company-profile-image"),
-    path('company-profiles/<slug>/create-company-profile-image/', CompanyProfileImageCreateView.as_view(),
+    path('company-profiles/<company_profile_slug>/create-company-profile-image/', CompanyProfileImageCreateView.as_view(),
          name="create-company-profile-image")
 ]
 # As part of our culture we believe in good work practices, and that's what we do.

334d7d1a2e1d2091c8defe3dbb4ccdfb4107aeb8
Clifford <kingsleyclifford2016@gmail.com>
2021-11-16 19:36:46 +0000

    added lines of code

--- jobs/api/serializers.py
+++ jobs/api/serializers.py
@@ -4,19 +4,19 @@ from jobs.models import Job, Seeker
 
 
 class SeekerSerializer(serializers.ModelSerializer):
-    job = serializers.StringRelatedField(read_only=True)
     slug = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Seeker
-        fields = "__all__"
+        exclude = ["job"]
 
 
 class JobSerializer(serializers.ModelSerializer):
     slug = serializers.StringRelatedField(read_only=True)
-    seekers = SeekerSerializer(many=True, read_only=True)
+    # seekers = SeekerSerializer(many=True, read_only=True)
     date_posted = serializers.StringRelatedField(read_only=True)
+    # company = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Job
-        fields = "__all__"
+        exclude = ["company"]

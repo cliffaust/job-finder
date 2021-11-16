590b17a668a33e093e1d9fab010b7766dfbcab26
Clifford <kingsleyclifford2016@gmail.com>
2021-11-16 20:40:21 +0000

    added lines of code

--- jobs/api/views.py
+++ jobs/api/views.py
@@ -78,6 +78,7 @@ class SeekerCreateView(generics.CreateAPIView):
 
 class SeekerDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SeekerSerializer
+    permission_classes = [IsAuthenticated]
     lookup_field = "slug"
 
     def get_queryset(self):

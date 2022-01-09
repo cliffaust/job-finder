ccd9072bde4547b97ef457acf4cef1000884a575
Clifford <kingsleyclifford2016@gmail.com>
2022-01-09 00:57:33 +0000

    added lines of code

--- jobs/api/serializers.py
+++ jobs/api/serializers.py
@@ -13,7 +13,7 @@ class SeekerSerializer(serializers.ModelSerializer):
 
 class JobSerializer(serializers.ModelSerializer):
     slug = serializers.StringRelatedField(read_only=True)
-    num_applicant = serializers.SerializerMethodField()
+    num_applicants = serializers.SerializerMethodField()
     date_posted = serializers.StringRelatedField(read_only=True)
     company = serializers.StringRelatedField(read_only=True)
 
@@ -21,5 +21,5 @@ class JobSerializer(serializers.ModelSerializer):
         model = Job
         fields = "__all__"
 
-    def get_num_applicant(self, instance):
+    def get_num_applicants(self, instance):
         return instance.seekers.count()

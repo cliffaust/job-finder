b108293844a03c58bf8a58c40a21685cc6bdb4a9
Clifford <kingsleyclifford2016@gmail.com>
2022-01-16 00:38:24 +0000

    added lines of code

--- company/api/serializers.py
+++ company/api/serializers.py
@@ -12,7 +12,11 @@ class CompanyProfileSerializer(serializers.ModelSerializer):
     slug = serializers.StringRelatedField(read_only=True)
     company_images = CompanyProfileImageSerializer(many=True, read_only=True)
     user = serializers.StringRelatedField(read_only=True)
+    company_profile_image = serializers.SerializerMethodField()
 
     class Meta:
         model = CompanyProfile
         fields = "__all__"
+
+    def get_company_profile_image(self, instance):
+        return instance.user.profile_pic.url
